5efef30581f685e6c4b5feb37412438df893a33e
johnnque <johnn_patrick_que@dlsu.edu.ph>
2025-08-19 19:41:36 +0800

    Major Data Structure Changes
    Memory allocation adds every time user enters new class individually. Each day of the week will have its own dynamic array
    
    This resolves issue with printing

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 int main(){
 
+    initializeProgram();
+    
     int userChoice = 0; 
 
     do{
@@ -11,54 +13,34 @@ int main(){
 
         switch(userChoice){
             case 1:
-                
-                //TODO can't get the confirmation to work, but makeNewSchedule() function alone works fine
-                /*
-                printf("WARNING!!!  Pre-existing schedule will be deleted. Do you still wish to process[y/n]: ");
-                char confirmInitSched = getchar();
-                printf("why not working 0");
-                while(getchar() != '\n');
-
-                if(confirmInitSched == 'y'){
-                    printf("why not working 1");
-                    makeNewSchedule();
-                    
-                    printf("why not working 2");
-                }*/
-
-                makeNewSchedule();
+                addClass();
                 break;
 
-
             case 2:
-                addClass();
+                printf("remove class function here\n");
                 break;
             
             case 3:
-                printf("remove class function here\n");
-                break;
+                printf("edit class here\n");
+            break;
 
             case 4:
-                printf("edit class here\n");
-                break;
-
-            case 5:
                 checkCurrentSchedule(); //Test function should be replaced with formatted schedule func.
                 break;
 
-            case 6:
+            case 5:
                 printf("print user guide here\n");
                 break;
-            
-            case 7:
-                break; //while loop condition will handle quitting
+
+            case 6://while loop condition will handle quitting
+                break;    
 
             default: 
                 printf("Invalid Selection\n");
                 userChoice = 0; //To ensure going back to menu
                 break; 
         }
-    } while(userChoice != 7);
+    } while(userChoice != 6);
 
 
     // FOR TESTING QUITTING
@@ -68,7 +50,17 @@ int main(){
     scanf("%d", &testVar);
     */
 
-    free(courseArray);
-    courseArray = NULL;
+    free(arrayM);
+    free(arrayT);
+    free(arrayW);
+    free(arrayH);
+    free(arrayF);
+    free(arrayS);
+    arrayM = NULL;
+    arrayT = NULL;
+    arrayW = NULL;
+    arrayH = NULL;
+    arrayF = NULL;
+    arrayS = NULL;
     return 0;
 }
--- scheduleMakerFunctions.h
+++ scheduleMakerFunctions.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 // Expected + '\0' + '\n' + '2 More Char' 
 #define courseSize 11   //FDCNSYS
@@ -8,9 +9,20 @@
 #define roomSize 10         //AG1011
 
 // Declare Pointer 
-struct Course *courseArray;
+struct Course * arrayM;
+struct Course * arrayT;
+struct Course * arrayW;
+struct Course * arrayH;
+struct Course * arrayF;
+struct Course * arrayS;
 
-int scheduleSize = 0;
+int sizeM = 0;
+int sizeT = 0;
+int sizeW = 0;
+int sizeH = 0;
+int sizeF = 0;
+int sizeS = 0;
+
 
 // STRUCTURES
 struct Course{
@@ -24,6 +36,14 @@ struct Course{
 
 
 // FUNCTIONS
+void initializeProgram(){
+    arrayM = NULL;
+    arrayT = NULL;
+    arrayW = NULL;
+    arrayH = NULL;
+    arrayF = NULL;
+    arrayS = NULL;
+}
 
 int menuSelection(){
     int userChoice = 0;
@@ -32,13 +52,12 @@ int menuSelection(){
 
         printf("========SCHEDULE MAKER========\n");
         printf("Program Options: \n");
-        printf("1| MAKE NEW schedule \n");
-        printf("2| ENTER a new class \n");
-        printf("3| REMOVE a class \n");
-        printf("4| EDIT a class \n");
-        printf("5| SHOW SCHEDULE\n");
-        printf("6| HELP \n");
-        printf("7| QUIT PROGRAM\n");
+        printf("1| ENTER a new class \n");
+        printf("2| REMOVE a class \n");
+        printf("3| EDIT a class \n");
+        printf("4| SHOW SCHEDULE\n");
+        printf("5| HELP \n");
+        printf("6| QUIT PROGRAM\n");
         
         printf("ENTER OPTION NUMBER: ");
         scanf("%d", &userChoice);
@@ -53,20 +72,14 @@ int menuSelection(){
 }
 
 
-void makeNewSchedule(){
-    printf("ENTER NUMBER OF COURSES: ");
-    scanf("%d", &scheduleSize);
-    while(getchar() != '\n');
+void addClass(){
+    int invalidInputDetected = 0;
+    do{
+        struct Course* temp;
 
-    // Switched to calloc()
-    courseArray = calloc(scheduleSize, sizeof(struct Course)); // Allocate size based on user input
-    
-
-    // Get course input from user
-    int i;
-    for(i = 0; i<scheduleSize; i+=1){
+        // get class info
         struct Course tempCourse;
-
+        
         while(getchar() != '\n');
         // Course Code
         printf("COURSE CODE: ");
@@ -91,17 +104,19 @@ void makeNewSchedule(){
                 printf("Please enter only 1 or 2 for once or twice a week\n");
             }
 
+            
+
         }while(timesAWeek != 1 && timesAWeek != 2);
         
 
         printf("ENTER: M, T, W, H, F, S\n");
         printf("DAY 1: ");
-        tempCourse.day1 = getchar();
+        tempCourse.day1 = toupper(getchar());
         while(getchar() != '\n');
 
         if(timesAWeek == 2){
             printf("DAY 2: ");
-            tempCourse.day2 = getchar();
+            tempCourse.day2 = toupper(getchar());
             while(getchar() != '\n');
         }
         else{
@@ -112,80 +127,212 @@ void makeNewSchedule(){
         printf("ROOM: ");
         fgets(tempCourse.room, roomSize, stdin);
         
-        // Send tempCourse object into dynamic array
-        courseArray[i] = tempCourse;
-    }
+        
+        // ADD THE COURSE INTO MEMORY
+        switch(tempCourse.day1){
+            case 'M':
+                if(arrayM == NULL){
+                    sizeM = sizeM + 1;
+                    arrayM = calloc(sizeM, sizeof(struct Course)); 
+                }
+                else{
+                    struct Course * temp;
+                    sizeM = sizeM + 1;
+                    temp = realloc(arrayM, sizeof(struct Course)*sizeM);
+                    arrayM = temp; 
+                }
+                arrayM[sizeM - 1] = tempCourse;
+                invalidInputDetected = 0;
+                break;
 
-}
+            case 'T':
+                if(arrayT == NULL){
+                    sizeT = sizeT + 1;
+                    arrayT = calloc(sizeT, sizeof(struct Course)); 
+                }
+                else{
+                    struct Course * temp;
+                    sizeT = sizeT + 1;
+                    temp = realloc(arrayT, sizeof(struct Course)*sizeT);
+                    arrayT = temp; 
+                }
+                arrayT[sizeT - 1] = tempCourse;
+                invalidInputDetected = 0;
+                break;
 
+            case 'W':
+                if(arrayW == NULL){
+                    sizeW = sizeW + 1;
+                    arrayW = calloc(sizeW, sizeof(struct Course)); 
+                }
+                else{
+                    struct Course * temp;
+                    sizeW = sizeW + 1;
+                    temp = realloc(arrayW, sizeof(struct Course)*sizeW);
+                    arrayW = temp; 
+                }
+                arrayW[sizeW - 1] = tempCourse;
+                invalidInputDetected = 0;
+                break;
 
+            case 'H':
+                if(arrayH == NULL){
+                    sizeH = sizeH + 1;
+                    arrayH = calloc(sizeH, sizeof(struct Course)); 
+                }
+                else{
+                    struct Course * temp;
+                    sizeH = sizeH + 1;
+                    temp = realloc(arrayH, sizeof(struct Course)*sizeH);
+                    arrayH = temp; 
+                }
+                arrayH[sizeH - 1] = tempCourse;
+                invalidInputDetected = 0;
+                break;
 
-void addClass(){
-
-    // increment allocation 
-    scheduleSize = scheduleSize + 1;
-    struct Course* temp;
-    temp = realloc(courseArray, scheduleSize * sizeof(struct Course));
-
-    // get class info
-    struct Course tempCourse;
-    
-    while(getchar() != '\n');
-    // Course Code
-    printf("COURSE CODE: ");
-    fgets(tempCourse.course, courseSize, stdin);
-
-    // Section
-    printf("SECTION: ");
-    fgets(tempCourse.section, sectionSize, stdin);
-
-    // Time
-    printf("TIME [Enter 09:50 - 13:00]: ");
-    fgets(tempCourse.time, timeSize, stdin);
-
-    // Day 1 and Day 2
-    int timesAWeek = 0;
-    do{
-        printf("ONCE OR TWICE A WEEK [Enter 1 or 2]: ");
-        scanf("%d", &timesAWeek);
-        while(getchar() != '\n');
-
-        if(timesAWeek != 1 && timesAWeek != 2){
-            printf("Please enter only 1 or 2 for once or twice a week\n");
+            case 'F':
+                if(arrayF == NULL){
+                    sizeF = sizeF + 1;
+                    arrayF = calloc(sizeF, sizeof(struct Course)); 
+                }
+                else{
+                    struct Course * temp;
+                    sizeF = sizeF + 1;
+                    temp = realloc(arrayF, sizeof(struct Course)*sizeF);
+                    arrayF = temp; 
+                }
+                arrayF[sizeF - 1] = tempCourse;
+                invalidInputDetected = 0;
+                break;
+                
+            case 'S':
+                if(arrayS == NULL){
+                    sizeS = sizeS + 1;
+                    arrayS = calloc(sizeS, sizeof(struct Course)); 
+                }
+                else{
+                    struct Course * temp;
+                    sizeS = sizeS + 1;
+                    temp = realloc(arrayS, sizeof(struct Course)*sizeS);
+                    arrayS = temp; 
+                }
+                arrayS[sizeS - 1] = tempCourse;
+                invalidInputDetected = 0;
+                break;
+            default:
+                invalidInputDetected = 1;
+                printf("ERROR: Invalid input detected\nPlease fill up the course info again\n\n");
+                printf("Press [ENTER] to continue: ");
+                break;
         }
 
-        
+        // MEMORY ALLOCAITON FOR DAY 2 
+        if(tempCourse.day2 != '\0' ){
+            switch(tempCourse.day2){
+                case 'M':
+                    if(arrayM == NULL){
+                        sizeM = sizeM + 1;
+                        arrayM = calloc(sizeM, sizeof(struct Course)); 
+                    }
+                    else{
+                        struct Course * temp;
+                        sizeM = sizeM + 1;
+                        temp = realloc(arrayM, sizeof(struct Course)*sizeM);
+                        arrayM = temp; 
+                    }
+                    arrayM[sizeM - 1] = tempCourse;
+                    invalidInputDetected = 0;
+                    break;
 
-    }while(timesAWeek != 1 && timesAWeek != 2);
-    
+                case 'T':
+                    if(arrayT == NULL){
+                        sizeT = sizeT + 1;
+                        arrayT = calloc(sizeT, sizeof(struct Course)); 
+                    }
+                    else{
+                        struct Course * temp;
+                        sizeT = sizeT + 1;
+                        temp = realloc(arrayT, sizeof(struct Course)*sizeT);
+                        arrayT = temp; 
+                    }
+                    arrayT[sizeT - 1] = tempCourse;
+                    invalidInputDetected = 0;
+                    break;
 
-    printf("ENTER: M, T, W, H, F, S\n");
-    printf("DAY 1: ");
-    tempCourse.day1 = getchar();
-    while(getchar() != '\n');
+                case 'W':
+                    if(arrayW == NULL){
+                        sizeW = sizeW + 1;
+                        arrayW = calloc(sizeW, sizeof(struct Course)); 
+                    }
+                    else{
+                        struct Course * temp;
+                        sizeW = sizeW + 1;
+                        temp = realloc(arrayW, sizeof(struct Course)*sizeW);
+                        arrayW = temp; 
+                    }
+                    arrayW[sizeW - 1] = tempCourse;
+                    invalidInputDetected = 0;
+                    break;
 
-    if(timesAWeek == 2){
-        printf("DAY 2: ");
-        tempCourse.day2 = getchar();
-        while(getchar() != '\n');
-    }
-    else{
-        tempCourse.day2 = '\0';
-    }
+                case 'H':
+                    if(arrayH == NULL){
+                        sizeH = sizeH + 1;
+                        arrayH = calloc(sizeH, sizeof(struct Course)); 
+                    }
+                    else{
+                        struct Course * temp;
+                        sizeH = sizeH + 1;
+                        temp = realloc(arrayH, sizeof(struct Course)*sizeH);
+                        arrayH = temp; 
+                    }
+                    arrayH[sizeH - 1] = tempCourse;
+                    invalidInputDetected = 0;
+                    break;
 
-    // Room
-    printf("ROOM: ");
-    fgets(tempCourse.room, roomSize, stdin);
-    
-    // Send tempCourse object into dynamic array
-    courseArray[scheduleSize - 1] = tempCourse;
+                case 'F':
+                    if(arrayF == NULL){
+                        sizeF = sizeF + 1;
+                        arrayF = calloc(sizeF, sizeof(struct Course)); 
+                    }
+                    else{
+                        struct Course * temp;
+                        sizeF = sizeF + 1;
+                        temp = realloc(arrayF, sizeof(struct Course)*sizeF);
+                        arrayF = temp; 
+                    }
+                    arrayF[sizeF - 1] = tempCourse;
+                    invalidInputDetected = 0;
+                    break;
+                    
+                case 'S':
+                    if(arrayS == NULL){
+                        sizeS = sizeS + 1;
+                        arrayS = calloc(sizeS, sizeof(struct Course)); 
+                    }
+                    else{
+                        struct Course * temp;
+                        sizeS = sizeS + 1;
+                        temp = realloc(arrayS, sizeof(struct Course)*sizeS);
+                        arrayS = temp; 
+                    }
+                    arrayS[sizeS - 1] = tempCourse;
+                    invalidInputDetected = 0;
+                    break;
+                default:
+                    invalidInputDetected = 1;
+                    printf("ERROR: Invalid input detected\nPlease fill up the course info again\n\n");
+                    printf("Press [ENTER] to continue: ");
+                    break;
+            }
+        }
 
-
+    }while(invalidInputDetected == 1);
     return;
 }
 
 void checkCurrentSchedule(){ // Currently To Check contents of dynamic array but eventually should print tabulated schedule
     int i;
+    /*
     for(i = 0; i<scheduleSize; i+=1){
         printf("COURSE: %s\n", courseArray[i].course);
         printf("SECTION: %s\n", courseArray[i].section);
@@ -194,6 +341,7 @@ void checkCurrentSchedule(){ // Currently To Check contents of dynamic array but
         printf("DAY2: %c\n", courseArray[i].day2);
         printf("ROOM: %s\n", courseArray[i].room);
     }
+    */
     return;
 }
     
