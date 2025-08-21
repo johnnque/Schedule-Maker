876a6241674881269baa59a2e023b39f12ed3e40
johnnque <johnn_patrick_que@dlsu.edu.ph>
2025-08-21 16:06:07 +0800

    main2_CompleteOverhaul

---
+++ variablesAndStructures.h
@@ -0,0 +1,13 @@
+
+
+struct Course{
+    char code[9];       //FDCNSYS
+    char section[6];    //ZZ14
+    char time[10];      //0900-1300
+    
+    // M = 1, T = 2, W = 3, H = 4, F = 5, S = 6
+    int day1;
+    int day2;
+    char room[8];       // ER1011
+};
+
